4a569331b87fc25ba35fe74f210551d9572eae98
Andi Péter <andipeti2000@gmail.com>
2023-07-01 22:45:53 +0200

    Remove old files

--- .old/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.experiments.topLevelAwait = true;
-        return config;
-    },
-};
